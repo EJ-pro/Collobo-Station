b25746ae2ab44672697cc5d9fe90a6edc208dde5
EJ-pro <hsshss2522@naver.com>
2024-04-18 20:05:18 +0900

    viewpager2 firebase 연동

---
+++ app/src/main/java/com/example/collobo_station/Adapter/ViewPager2Adapter.kt
@@ -0,0 +1,52 @@
+package com.example.collobo_station.Adapter
+
+import android.graphics.BitmapFactory
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.collobo_station.R
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+class ViewPager2Adapter(private var imageUrls: List<String>, private val context: android.content.Context) : RecyclerView.Adapter<ViewPager2Adapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.viewpager2_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageUrl = imageUrls[position]
+        holder.bind(imageUrl)
+    }
+
+    override fun getItemCount(): Int {
+        return imageUrls.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.image)
+
+        fun bind(imageUrl: String) {
+            val storageReference = Firebase.storage.reference.child(imageUrl)
+
+            storageReference.getBytes(Long.MAX_VALUE)
+                .addOnSuccessListener { bytes ->
+                    // 이미지 다운로드 성공 시 비트맵으로 변환하여 ImageView에 설정
+                    val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    imageView.setImageBitmap(bmp)
+                }
+                .addOnFailureListener { exception ->
+                    // 이미지 다운로드 실패 시 로그 기록
+                    Log.e("ViewPager2Adapter", "Error downloading image", exception)
+                }
+        }
+    }
+}
--- app/src/main/java/com/example/collobo_station/Fragment/Fragment_Home.kt
+++ app/src/main/java/com/example/collobo_station/Fragment/Fragment_Home.kt
@@ -1,7 +1,5 @@
 package com.example.collobo_station.Fragment
 
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,48 +7,67 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
-import com.example.collobo_station.Adapter.ViewPager2Adater
+import com.example.collobo_station.Adapter.ViewPager2Adapter
 import com.example.collobo_station.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Fragment_Home : Fragment() {
 
-    var layoutOnBoardingIndicators: LinearLayout? = null
+    private lateinit var viewPager: ViewPager2
+    private lateinit var adapter: ViewPager2Adapter
+    private lateinit var layoutOnBoardingIndicators: LinearLayout
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        // ViewPager2 관련 코드 이동
-        val list = ArrayList<Int>()
-        list.add(Color.parseColor("#ffff00"))
-        list.add(Color.parseColor("#bdbdbd"))
-        list.add(Color.parseColor("#0f9231"))
-        val adapter = ViewPager2Adater(list, requireContext())
-
-        val viewPager = view.findViewById<ViewPager2>(R.id.viewpager2)
-        viewPager.offscreenPageLimit = 3
-        viewPager.getChildAt(0).overScrollMode = View.OVER_SCROLL_NEVER
-        viewPager.adapter = adapter
-
+        viewPager = view.findViewById(R.id.viewpager2)
         layoutOnBoardingIndicators = view.findViewById(R.id.indicators)
-        setupOnBoardingIndicators()
-        setCurrentOnboardingIndicator(0)
+
+        // Firestore에서 Recommendation Contest 컬렉션의 문서 개수 가져오기
+        val firestore = FirebaseFirestore.getInstance()
+        firestore.collection("Recommendation Contest")
+            .get()
+            .addOnSuccessListener { documents ->
+                val imageUrls = ArrayList<String>()
+
+                // 각 문서에서 이미지의 위치 가져오기
+                for (document in documents) {
+                    val imageUrl = document.getString("이미지")
+                    imageUrl?.let {
+                        imageUrls.add(it)
+                    }
+                }
+
+                // ViewPager2Adapter에 이미지 URL 리스트 전달하여 어댑터 생성
+                adapter = ViewPager2Adapter(imageUrls, requireContext())
+                viewPager.adapter = adapter
+
+                // 인디케이터 설정
+                setupOnBoardingIndicators()
+                setCurrentOnboardingIndicator(1)
+            }
+            .addOnFailureListener { exception ->
+                // Firestore에서 데이터를 가져오는 데 실패한 경우 처리
+                // 예를 들어, Log.e()를 사용하여 오류를 기록하거나 사용자에게 알림을 표시할 수 있습니다.
+            }
 
         val transform = CompositePageTransformer()
-        transform.addTransformer(MarginPageTransformer(8))
-
+        val pageMargin = resources.getDimensionPixelOffset(R.dimen.pageMargin)
+        val pageOffset = resources.getDimensionPixelOffset(R.dimen.pageOffset)
+        transform.addTransformer(MarginPageTransformer(pageMargin))
         transform.addTransformer(ViewPager2.PageTransformer { view: View, fl: Float ->
             val v = 1 - Math.abs(fl)
             view.scaleY = 0.8f + v * 0.2f
         })
+        viewPager.offscreenPageLimit = 1
 
         viewPager.setPageTransformer(transform)
 
@@ -63,13 +80,13 @@ class Fragment_Home : Fragment() {
         return view
     }
 
+    // 인디케이터 설정 메서드
     private fun setupOnBoardingIndicators() {
-        val indicators = arrayOfNulls<ImageView>(3)
-
+        val indicators = arrayOfNulls<ImageView>(adapter.itemCount)
         val layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
         )
-
         layoutParams.setMargins(8, 0, 8, 0)
 
         for (i in indicators.indices) {
@@ -80,32 +97,21 @@ class Fragment_Home : Fragment() {
                     R.drawable.viewpager2_background_color
                 )
             )
-
             indicators[i]?.layoutParams = layoutParams
-
-            layoutOnBoardingIndicators?.addView(indicators[i])
+            layoutOnBoardingIndicators.addView(indicators[i])
         }
     }
 
+    // 현재 선택된 인디케이터 설정 메서드
     private fun setCurrentOnboardingIndicator(index: Int) {
-        val childCount = layoutOnBoardingIndicators?.childCount
-        for (i in 0 until childCount!!) {
-            val imageView = layoutOnBoardingIndicators?.getChildAt(i) as ImageView
-            if (i == index) {
-                imageView.setImageDrawable(
-                    ContextCompat.getDrawable(
-                        requireContext(),
-                        R.drawable.viewpager2_background_color
-                    )
+        for (i in 0 until layoutOnBoardingIndicators.childCount) {
+            val imageView = layoutOnBoardingIndicators.getChildAt(i) as ImageView
+            imageView.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    if (i == index) R.drawable.viewpager2_background_color else R.drawable.viewpager2_background_color
                 )
-            } else {
-                imageView.setImageDrawable(
-                    ContextCompat.getDrawable(
-                        requireContext(),
-                        R.drawable.viewpager2_background_color
-                    )
-                )
-            }
+            )
         }
     }
 }
