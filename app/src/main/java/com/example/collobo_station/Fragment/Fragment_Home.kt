12369c860ce98bb39c399f16f16c29fefcab4b28
EJ-pro <hsshss2522@naver.com>
2024-04-26 07:36:36 +0900

    fragment_home 메소드화

--- app/src/main/java/com/example/collobo_station/Fragment/Fragment_Home.kt
+++ app/src/main/java/com/example/collobo_station/Fragment/Fragment_Home.kt
@@ -37,7 +37,7 @@ class Fragment_Home : Fragment() {
     private lateinit var adapter: ViewPager2Adapter
     private lateinit var layoutOnBoardingIndicators: LinearLayout
     private lateinit var textViewContestName: Button
-    private lateinit var textViewUserName:TextView
+    private lateinit var textViewUserName: TextView
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPagerTabs: ViewPager2
     private lateinit var tabadapter: ViewPager2Adapter
@@ -49,17 +49,26 @@ class Fragment_Home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        initView(view)
+        fetchUserInfo()
+        setupViewPager()
+        setupTabLayout(view)
+        return view
+    }
+
+    private fun initView(view: View) {
         viewPager = view.findViewById(R.id.viewpager2)
         layoutOnBoardingIndicators = view.findViewById(R.id.indicators)
         textViewContestName = view.findViewById(R.id.textViewContestName)
         textViewUserName = view.findViewById(R.id.textViewUserName)
+    }
 
+    private fun fetchUserInfo() {
         val user = Firebase.auth.currentUser
         if (user != null) {
             val userEmail = user.email
             if (userEmail != null) {
                 val db = Firebase.firestore
-                // Users 컬렉션에서 현재 사용자의 이메일과 일치하는 문서를 찾습니다.
                 db.collection("Users")
                     .whereEqualTo("email", userEmail)
                     .get()
@@ -68,32 +77,33 @@ class Fragment_Home : Fragment() {
                             val document = documents.documents[0]
                             val nickname = document.getString("nickname")
                             if (nickname != null) {
-                                // 닉네임이 존재하는 경우에만 표시합니다.
                                 textViewUserName.text = nickname
-                                Toast.makeText(requireContext(), "안녕하세요, $nickname 님!", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    requireContext(),
+                                    "안녕하세요, $nickname 님!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             } else {
-                                // 닉네임이 없는 경우 "사용자"로 표시합니다.
                                 textViewUserName.text = "사용자"
                             }
                         } else {
-                            // 일치하는 문서가 없는 경우 "사용자"로 표시합니다.
                             textViewUserName.text = "사용자"
                         }
                     }
                     .addOnFailureListener { e ->
                         Log.e(TAG, "Error getting document", e)
-                        // 오류 발생 시 "로그인 필요"로 표시합니다.
                         textViewUserName.text = "로그인 필요"
                     }
             } else {
-                // 사용자 이메일이 없는 경우 "로그인 필요"로 표시합니다.
                 textViewUserName.text = "로그인 필요"
             }
         } else {
-            // 사용자가 로그인하지 않은 경우 "로그인 필요"로 표시합니다.
             textViewUserName.text = "로그인 필요"
         }
+    }
 
+
+    private fun setupViewPager() {
         val firestore = FirebaseFirestore.getInstance()
         firestore.collection("Recommendation Contest")
             .get()
@@ -131,38 +141,6 @@ class Fragment_Home : Fragment() {
                 setCurrentOnboardingIndicator(position)
             }
         })
-
-        tabLayout = view.findViewById(R.id.tabLayout)
-        viewPagerTabs = view.findViewById(R.id.view_pager)
-
-        var tabAdapter = TabAdapter(childFragmentManager, lifecycle)
-
-        tabAdapter.addFragment(Fragment_Tab_All(), "전체보기")
-        tabAdapter.addFragment(Fragment_Tab_Recent(), "최근등록순")
-        tabAdapter.addFragment(Fragment_Tab_DeadLine(),"마감순")
-
-        viewPagerTabs.adapter = tabAdapter
-
-        TabLayoutMediator(tabLayout, viewPagerTabs) { tab, position ->
-            tab.text = tabAdapter.getPageTitle(position)
-        }.attach()
-
-        viewPagerTabs.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                val fragment = getChildFragmentManager().fragments[position]
-                if (fragment is Fragment_Tab_All) {
-                    fragment.adjustRecyclerViewSize()
-                } else if (fragment is Fragment_Tab_Recent) {
-                    fragment.adjustRecyclerViewSize()
-                } else if (fragment is Fragment_Tab_DeadLine) {
-                    fragment.adjustRecyclerViewSize()
-                }
-            }
-        })
-
-
-        return view
     }
 
     private fun setupOnBoardingIndicators() {
@@ -212,7 +190,7 @@ class Fragment_Home : Fragment() {
                         val targetAudience = it.getString("응모대상") ?: ""
                         val receptionPeriod = it.getString("접수기간") ?: ""
                         val totalPrize = it.getString("총상금") ?: ""
-                        val url = it.getString("url") ?:""
+                        val url = it.getString("url") ?: ""
 
                         textViewContestName.setOnClickListener {
                             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
@@ -226,4 +204,34 @@ class Fragment_Home : Fragment() {
             }
     }
 
+    private fun setupTabLayout(view: View) {
+        tabLayout = view.findViewById(R.id.tabLayout)
+        viewPagerTabs = view.findViewById(R.id.view_pager)
+
+        var tabAdapter = TabAdapter(childFragmentManager, lifecycle)
+
+        tabAdapter.addFragment(Fragment_Tab_All(), "전체보기")
+        tabAdapter.addFragment(Fragment_Tab_Recent(), "최근등록순")
+        tabAdapter.addFragment(Fragment_Tab_DeadLine(), "마감순")
+
+        viewPagerTabs.adapter = tabAdapter
+
+        TabLayoutMediator(tabLayout, viewPagerTabs) { tab, position ->
+            tab.text = tabAdapter.getPageTitle(position)
+        }.attach()
+
+        viewPagerTabs.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                val fragment = getChildFragmentManager().fragments[position]
+                if (fragment is Fragment_Tab_All) {
+                    fragment.adjustRecyclerViewSize()
+                } else if (fragment is Fragment_Tab_Recent) {
+                    fragment.adjustRecyclerViewSize()
+                } else if (fragment is Fragment_Tab_DeadLine) {
+                    fragment.adjustRecyclerViewSize()
+                }
+            }
+        })
+    }
 }
