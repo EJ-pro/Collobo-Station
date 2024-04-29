5f3bbe05678d1f3220c283f32de080df64e302f7
EJ-pro <hsshss2522@naver.com>
2024-04-29 22:47:04 +0900

    add sharebtn

--- app/src/main/java/com/example/collobo_station/Fragment/Tab/ContestDetailActivity.kt
+++ app/src/main/java/com/example/collobo_station/Fragment/Tab/ContestDetailActivity.kt
@@ -92,6 +92,18 @@ class ContestDetailActivity : AppCompatActivity() {
         contestEligibilityTextView.text = contestEligibility
         contestHomepageUrlTextView.text = contestHomepageUrl
         contestprecautionsTextView.text = contestprecautions
+
+        val shareBtn: Button = findViewById(R.id.shareBtn)
+        shareBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND_MULTIPLE)
+            intent.type = "text/plain"
+            val blogUrl = contestHomepageUrl
+            val content = "친구가 링크를 공유했어요!\n어떤 링크인지 들어가서 확인해볼까요?"
+            intent.putExtra(Intent.EXTRA_TEXT,"$content\n\n$blogUrl")
+
+            val chooserTitle = "친구에게 공유하기"
+            startActivity(Intent.createChooser(intent, chooserTitle))
+        }
     }
     // 웹페이지 열기
     private fun openWebPage(url: String) {
